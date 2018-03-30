1e4ea4645ae7f4d9877b70f1b12863d10f8510bc
Edward Fallon <eddie_fallon@hotmail.com>
2018-03-30 14:16:52 +0100

    added the add or remove cash method and pets sold method

--- pet_shop.rb
+++ pet_shop.rb
@@ -5,3 +5,11 @@ end
 def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop, num)
+  return pet_shop[:admin][:total_cash] += num
+end
+
+def pets_sold(pet_shop)
+  return pet_shop[:admin][:pets_sold]
+end 
